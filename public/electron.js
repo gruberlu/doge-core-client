99eba46fbba2450b470a5bb938b53964c8254867
Lukas Gruber <grbr.lks@googlemail.com>
2021-05-12 10:55:25 +0200

    initial commit

---
+++ public/electron.js
@@ -0,0 +1,58 @@
+const path = require('path')
+
+const { app, BrowserWindow } = require('electron')
+const isDev = require('electron-is-dev')
+
+// Conditionally include the dev tools installer to load React Dev Tools
+let installExtension, REACT_DEVELOPER_TOOLS
+
+if (isDev) {
+  const devTools = require("electron-devtools-installer");
+  installExtension = devTools.default
+  REACT_DEVELOPER_TOOLS = devTools.REACT_DEVELOPER_TOOLS
+}
+
+// Handle creating/removing shortcuts on Windows when installing/uninstalling
+if (require("electron-squirrel-startup")) {
+    app.quit()
+}
+
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+        // preload: path.join(__dirname, 'preload.js'),
+        nodeIntegration: true,
+    }
+  })
+
+  isDev ? win.loadURL('http://localhost:3000') : win.loadFile('index.html')
+
+  // Open the DevTools.
+  if (isDev) {
+    win.webContents.openDevTools({ mode: "detach" })
+  }
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  if (isDev) {
+    installExtension(REACT_DEVELOPER_TOOLS)
+      .then(name => console.log(`Added Extension:  ${name}`))
+      .catch(error => console.log(`An error occurred: , ${error}`))
+  }
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
