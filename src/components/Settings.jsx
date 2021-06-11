6091263d6c1845287d628deb6ca4ef82dd76d7ba
Lukas Gruber <grbr.lks@googlemail.com>
2021-06-11 15:21:53 +0200

    added switch, new button styling

--- src/components/Settings.jsx
+++ src/components/Settings.jsx
@@ -1,19 +1,23 @@
-import {useState, useEffect} from 'react'
-import {Switch, Card, Snackbar} from '@material-ui/core'
+import { useState, useEffect } from 'react'
+import { Card, Snackbar } from '@material-ui/core'
 import MuiAlert from '@material-ui/lab/Alert'
 
 export const Settings = () => {
-    const [theme, setTheme] = useState(window.electron.getTheme().then(theme => theme))
-    const [username, setUsername] = useState()
-    const [password, setPassword] = useState()
-    const [host, setHost] = useState()
-    const [port, setPort] = useState()
+    const [theme, setTheme] = useState(false)
+    const [username, setUsername] = useState('usr')
+    const [password, setPassword] = useState('pass')
+    const [host, setHost] = useState('localhost')
+    const [port, setPort] = useState(12345)
     const [error, setError] = useState(null)
     const [result, setResult] = useState(null)
 
     useEffect(() => {
         const fetchCreds = async () => {
             const creds = await window.electron.getCredentials()
+            const theme = await window.electron.getTheme()
+
+            setTheme(theme)
+
             setUsername(creds.username)
             setPassword(creds.password)
             setHost(creds.host)
@@ -34,16 +38,16 @@ export const Settings = () => {
 
     const handleClose = (event, reason) => {
         if (reason === 'clickaway') {
-          return;
+            return;
         }
-    
+
         setError(null)
         setResult(null)
     }
 
     const submitForm = (e) => {
         e.preventDefault()
-        if(username === "" || password === "" || host === "" || port === ""){
+        if (username === "" || password === "" || host === "" || port === "") {
             setError(true)
         }
         else {
@@ -63,22 +67,12 @@ export const Settings = () => {
             <Card className="Card">
                 <form>
                     <h1>Settings</h1>
-                    {/* <Switch
-                        checked={theme}
-                        onChange={handleChange}
-                        name="setTheme"
-                        inputProps={{ 'aria-label': 'secondary checkbox' }}
-                    />
-                    <label>Dark Mode</label> */}
-                    {/* <Inputfield id="username" label="Username" required onChange={(value) => {setUsername(value)}} />
-                    <Inputfield id="password" label="Password" type="password" required onChange={(value) => {setPassword(value)}} />
-                    <Inputfield id="host" label="Host" required onChange={(value) => {setHost(value)}} />
-                    <Inputfield id="port" label="Port" required onChange={(value) => {setPort(value)}} /> */}
-                    <div><label>Username:</label><input value={username} required={true} onChange={(e) => {setUsername(e.target.value)}}/></div>
-                    <div><label>Password:</label><input value={password} required type="password" onChange={(e) => {setPassword(e.target.value)}}/></div>
-                    <div><label>Host:</label><input value={host} required onChange={(e) => {setHost(e.target.value)}}/></div>
-                    <div><label>Port:</label><input value={port} required onChange={(e) => {setPort(e.target.value)}}/></div>
-                    <button type="submit" onClick={(e) => {submitForm(e)}}>Submit</button>
+                    <div><label>Darkmode:</label><input id="switch" type="checkbox" checked={theme} onChange={() => handleChange()}></input><label className="switch-label" htmlFor="switch"></label></div>
+                    <div><label>Username:</label><input value={username} required={true} onChange={(e) => { setUsername(e.target.value) }} /></div>
+                    <div><label>Password:</label><input value={password} required type="password" onChange={(e) => { setPassword(e.target.value) }} /></div>
+                    <div><label>Host:</label><input value={host} required onChange={(e) => { setHost(e.target.value) }} /></div>
+                    <div><label>Port:</label><input value={port} required onChange={(e) => { setPort(e.target.value) }} /></div>
+                    <button type="submit" onClick={(e) => { submitForm(e) }}>Submit</button>
                 </form>
             </Card>
             <Snackbar className="snackbar" autoHideDuration={6000} open={result} onClose={handleClose}>
